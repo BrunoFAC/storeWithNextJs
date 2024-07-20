d387b8982de72af4c880d98e31c9fde28b32ce75
Almeida <brunoalmeida15032001@gmail.com>
2024-07-20 04:16:16 +0100

    added notifications on favorites

--- src/components/addToCartFloatButton/addToCartFloatButton.tsx
+++ src/components/addToCartFloatButton/addToCartFloatButton.tsx
@@ -4,8 +4,11 @@ import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 import { FC } from 'react';
 import { useMarketStore } from '../../store';
 import { resources } from '../../global/resources';
+import { useSnackbar } from 'notistack';
 
 export const AddToCartFloatButton: FC = () => {
+    const { enqueueSnackbar } = useSnackbar();
+
     const scrolled = useScrollTrigger({ disableHysteresis: true, threshold: 0 });
     const cart = useMarketStore((store) => store.cart);
     const favorites = useMarketStore((store) => store.favorites);
@@ -18,10 +21,16 @@ export const AddToCartFloatButton: FC = () => {
     const allFavoritesAreOnCart = favoritesIds.every((id) => cartIds.includes(id));
 
     const addAllToCart = () => {
+        enqueueSnackbar(resources.hasBeenAddedAllFavoritesToCart, {
+            variant: 'success',
+        });
         const favoritesNotIncludedInCart = favorites.filter((id) => !cart.includes(id));
         setCart([...cart, ...favoritesNotIncludedInCart]);
     };
     const removeAllToCart = () => {
+        enqueueSnackbar(resources.hasBeenRemovedAllFavoritesFromCart, {
+            variant: 'success',
+        });
         const favoritesNotIncludedInCart = cart.filter((id) => !favorites.includes(id));
         setCart([...favoritesNotIncludedInCart]);
     };
